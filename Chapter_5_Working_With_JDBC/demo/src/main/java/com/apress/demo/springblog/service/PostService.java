78b383b372c571b12f2f66b0d9602e23d65a1601
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-07-11 17:30:11 -0500

    Starting on the section on using Jdbc Template With Spring Boot

--- Chapter_5_Working_With_JDBC/demo/src/main/java/com/apress/demo/springblog/service/PostService.java
+++ Chapter_5_Working_With_JDBC/demo/src/main/java/com/apress/demo/springblog/service/PostService.java
@@ -1,19 +1,22 @@
 package com.apress.demo.springblog.service;
 
 import com.apress.demo.springblog.domain.Post;
-import com.apress.demo.springblog.repository.PostRepository;
+import com.apress.demo.springblog.repository.JdbcPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
-    private final PostRepository postRepository;
+    private final JdbcPostRepository postRepository;
 
     public void addPost(Post post){
+        post.setCreatedOn(LocalDate.now());
+        post.setUpdatedOn(LocalDate.now());
         postRepository.addPost(post);
     }
 
